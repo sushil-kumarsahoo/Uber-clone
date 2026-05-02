4c7cc37cb615718f628db34806f937d25b096c88
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-05-02 11:07:15 +0530

    create a http server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
+const dotenv = require('dotenv'); 
+dotenv.config()
+const cors = require('cors');
 const express = require('express');
+const app = express();
+
+app.use(cors());
+
+
+app.get('/', (req,res) => {
+    res.send('hello world');
+});
+
+module.exports = app;
