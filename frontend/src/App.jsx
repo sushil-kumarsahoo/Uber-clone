ca18828b5960d1382f6487a931c11a262266be63
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-05-11 12:20:30 +0530

    created pages and context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,10 @@ import UserSignup from './pages/UserSignup'
 import UserLogin from './pages/UserLogin'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
+import { useContext } from 'react'
+import { UserDataContext } from './context/UserContext'
 function App() {
+
 
 
   return (
