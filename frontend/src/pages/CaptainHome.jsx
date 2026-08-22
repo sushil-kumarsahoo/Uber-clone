af9e9c3ceaaa1430669bc4b34e5bed17e78c7749
sushil-kumarsahoo <sushilkmrsho@gmail.com>
2026-08-22 16:34:02 +0530

    Captain home-page

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,7 +1,45 @@
 import bgImage from "../assets/ChatGPT Image May 13, 2026, 06_04_54 PM.png";
 import { Link } from "react-router-dom";
+import CaptainDetail from "../components/CaptainDetail";
+import RidePopup from "../components/RidePopup";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef, useState } from "react";
+import ConfirmRidePopup from "../components/ConfirmRidePopup";
 
 const CaptainHome = () => {
+
+  const ridePopupPanelRef = useRef(null)
+  const ConfirmRidePopupPanelRef = useRef(null)
+  const [ridePopupPanel, setRidePopupPanel] = useState(true)
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false)
+
+  useGSAP(function () {
+    if (ridePopupPanel) {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(ridePopupPanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [ridePopupPanel]);
+
+  useGSAP(function () {
+    if (confirmRidePopupPanel) {
+      gsap.to(ConfirmRidePopupPanelRef.current, {
+        transform: "translateY(0)",
+      });
+    } else {
+      gsap.to(ConfirmRidePopupPanelRef.current, {
+        transform: "translateY(100%)",
+      });
+    }
+  }, [confirmRidePopupPanel]);
+
+
+
   return (
     <div className="h-screen">
       <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
@@ -21,44 +59,21 @@ const CaptainHome = () => {
       <div className="h-3/5">
         <img className="h-full w-full object-cover" src={bgImage} alt="" />
       </div>
-      <div className="h-2/5 p-6">
-        <div className="flex items-center justify-between">
-          <div className="flex ite justify-start gap-4">
-            <img
-              className="h-10 w-10 rounded-full object-cover"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1O__BhAqnmH8mYeeBakpmI0BXy0nEGaZlEoP-evyHOg&s"
-              alt=""
-            />
-            <h4 className="text-lg font-medium">Sushil</h4>
-          </div>
-          <div>
-            <h4 className="text-xl font-semibold">₹295.2</h4>
-            <p className="text-sm text-gray-600">Earned</p>
-          </div>
-        </div>
 
-        <div className="flex mt-8 p-3 bg-gray-200 rounded-full justify-center gap-5 items-start">
-          <div className="text-center ">
-            <i className="text-3xl mb-2 font-thin ri-time-line"></i>
-            <h5 className="text-lg font-medium">10.2</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-          <div className="text-center">
-            <i className="text-3xl mb-2 font-thin ri-speed-up-fill"></i>
-            <h5 className="text-lg font-medium">10.2</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-          <div className="text-center">
-            <i className="text-3xl mb-2 font-thin ri-sticky-note-line"></i>
-            <h5 className="text-lg font-medium">10.2</h5>
-            <p className="text-sm text-gray-600">Hours Online</p>
-          </div>
-        </div>
+      <div className="h-2/5 p-6">
+        <CaptainDetail />
       </div>
+
+      <div ref={ridePopupPanelRef} className="fixed z-10 bottom-0 bg-white px-3 translate-y-full py-10 pt-12 w-full">
+        <RidePopup setRidePopupPanel={setRidePopupPanel} setConfirmRidePopupPanel={setConfirmRidePopupPanel} />
+      </div>
+
+      <div ref={ConfirmRidePopupPanelRef} className="fixed h-screen z-10 bottom-0 bg-white px-3 translate-y-full py-10 pt-12 w-full">
+        <ConfirmRidePopup setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
+      </div>
+
     </div>
   );
 };
 
 export default CaptainHome;
-
-6.2;
